c0cf2dbac99475ccc44db36881aff992b6e56952
SalgotraRishav <salgotra_rishav@yahoo.in>
2025-06-26 22:52:11 -0400

    Adding files of project

--- youtubesearch/src/test/java/com/youtubeapp/youtubesearch/YoutubesearchApplicationTests.java
+++ youtubesearch/src/test/java/com/youtubeapp/youtubesearch/YoutubesearchApplicationTests.java
@@ -2,8 +2,10 @@ package com.youtubeapp.youtubesearch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "youtube.api.key=dummy-test-key")
 class YoutubesearchApplicationTests {
 
 	@Test
